a92d350f502b5852617075fe2dd9b3ab57a24515
AlexVolkov0404 <90132916+AlexVolkov0404@users.noreply.github.com>
2023-09-28 18:32:10 +0300

    added lab3 a

---
+++ Lab3/Lab3_a/src/BearThread.java
@@ -0,0 +1,17 @@
+public class BearThread extends Thread {
+
+    private final Bucket bucket;
+
+    public BearThread(Bucket bucket) {
+        this.bucket = bucket;
+    }
+
+    @Override
+    public void run() {
+        while (isAlive()) {
+            if (bucket.takeNotification()) {
+                bucket.eatAllHoney();
+            }
+        }
+    }
+}
