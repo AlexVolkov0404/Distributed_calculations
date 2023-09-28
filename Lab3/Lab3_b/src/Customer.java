0787e4ae8800041fe1ca37ab338b5b8a219a399a
AlexVolkov0404 <90132916+AlexVolkov0404@users.noreply.github.com>
2023-09-28 18:58:42 +0300

    add lab3 b

---
+++ Lab3/Lab3_b/src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer extends Thread {
+    private HairStyler hairStyler;
+
+    public void setHairStyler(HairStyler hairStyler) {
+        this.hairStyler = hairStyler;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            try {
+                System.out.println("[Customer] Came in, waiting");
+                synchronized (hairStyler) {
+                    hairStyler.notify();
+                }
+                wait();
+                System.out.println("[Customer] Taking a seat");
+                notify();
+                System.out.println("[Customer] Falling a sleep");
+                wait();
+                System.out.println("[Customer] Leaving");
+                notify();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
