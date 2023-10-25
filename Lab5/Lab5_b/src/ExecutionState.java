e6542a2a55f592fae948292699b9ab91af53b1ba
AlexVolkov0404 <90132916+AlexVolkov0404@users.noreply.github.com>
2023-10-25 21:11:12 +0300

    Lab5

---
+++ Lab5/Lab5_b/src/ExecutionState.java
@@ -0,0 +1,11 @@
+public class ExecutionState {
+    private boolean state = true;
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+}
